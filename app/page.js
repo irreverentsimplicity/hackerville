dff3130970a3dd03bb0743d3eda026151c9a38a5
irreverentsimplicity <irreverentsimplicity@proton.me>
2024-05-13 06:58:18 -0400

    change name, minor layout fixes

--- app/page.js
+++ app/page.js
@@ -2,9 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-20">
       <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-lg lg:flex">
-        <p className="fixed left-0 top-0 right-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed mb-4 left-0 top-0 right-0 flex w-full justify-center 
+        border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-4 
+        backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit 
+        lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           The first rule of the flip
         </p>
       </div>
@@ -21,12 +24,12 @@ export default function Home() {
       </div>
 
       <div className="flex-col z-10 w-full max-w-5xl items-center justify-center font-mono text-lg lg:flex">
-      <p className="fixed left-0 top-0 right-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Is that we don't talk about the flip
-      </p>
-      <p className="fixed text-sm left-0 top-0 right-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2 lg:dark:bg-zinc-800/30">
-          Just yet.
-      </p>
+        <p className="fixed mt-2 left-0 top-0 right-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Is that we don&apos;t talk about the flip
+        </p>
+        <p className="fixed mt-4 text-sm left-0 top-0 right-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-2 lg:dark:bg-zinc-800/30">
+            Just yet.
+        </p>
       </div>
     </main>
   )
